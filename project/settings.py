d6a755ecb7b8de60c892bfed119c78401fae5c41
zahraa Meky <eng.alzahraa.meky@gmail.com>
2023-10-27 11:36:33 +0400

    update settings

--- project/settings.py
+++ project/settings.py
@@ -70,24 +70,25 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
-# database_url = os.environ.get("DATABASE_URL")
+database_url = os.environ.get("DATABASE_URL")
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if not DEBUG:
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    DATABASES = {
-    'default': dj_database_url.config(),
-}
-else:
-    DATABASES = {
+# if not DEBUG:
+#     DATABASES = {
+#         'default': {
+#             dj_database_url.parse("database_url")
+#         }
+#     }
+# else:
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-# DATABASES['default'] = dj_database_url.parse("database_url")
+DATABASES['default'] = dj_database_url.parse("database_url")
 
 
 # Password validation
